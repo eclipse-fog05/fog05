6ccd74d051dad80f00abaf57ab7fe2c21afbc7bd
gabrik <gabriele.baldoni@gmail.com>
2018-09-04 11:54:06 +0200

    double licensing update

--- bin/f05wc.bat
+++ bin/f05wc.bat
@@ -5,9 +5,10 @@ REM # information regarding copyright ownership.
 REM # 
 REM # This program and the accompanying materials are made available under the
 REM # terms of the Eclipse Public License 2.0 which is available at
-REM # http://www.eclipse.org/legal/epl-2.0
-REM # 
-REM # SPDX-License-Identifier: EPL-2.0
+REM # http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+REM # which is available at https://www.apache.org/licenses/LICENSE-2.0.
+REM #
+REM # SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
 REM #
 REM # Contributors: Gabriele Baldoni, ADLINK Technology Inc. - Initial implementation and API
 
