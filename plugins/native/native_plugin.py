8fbc477791ea74516ea855d41505eb45082e4d7d
gabrik <gabriele.baldoni@gmail.com>
2018-10-01 10:26:28 +0200

    changed ifs in state change reaction on native plugin
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- plugins/native/native_plugin.py
+++ plugins/native/native_plugin.py
@@ -517,52 +517,42 @@ class Native(RuntimePlugin):
     def __react_to_cache(self, uri, value, v):
         self.agent.logger.info('__react_to_cache()', ' Native Plugin - React to to URI: {} Value: {} Version: {}'.format(uri, value, v))
         if uri.split('/')[-2] == 'entity':
-            if value is None and v is None:
+            uuid = uri.split('/')[-1]
+            value = json.loads(value)
+            action = value.get('status')
+            entity_data = value.get('entity_data')
+            react_func = self.__react(action)
+            if action == 'undefine':
                 self.agent.logger.info('__react_to_cache()', ' Native Plugin - This is a remove for URI: {}'.format(uri))
-                entity_uuid = uri.split('/')[-1]
-                self.undefine_entity(entity_uuid)
-            else:
-                uuid = uri.split('/')[-1]
-                value = json.loads(value)
-                action = value.get('status')
-                entity_data = value.get('entity_data')
-                react_func = self.__react(action)
-                if action == 'undefine':
-                    self.undefine_entity(uuid)
-                elif react_func is not None and entity_data is None:
-                    react_func(uuid)
-                elif react_func is not None:
-                    entity_data.update({'entity_uuid': uuid})
-                    if action == 'define':
-                        react_func(**entity_data)
-                    else:
-                        react_func(entity_data)
+                self.undefine_entity(uuid)
+            elif react_func is not None and entity_data is None:
+                react_func(uuid)
+            elif react_func is not None:
+                entity_data.update({'entity_uuid': uuid})
+                if action == 'define':
+                    react_func(**entity_data)
+                else:
+                    react_func(entity_data)
         elif uri.split('/')[-2] == 'instance':
-            if value is None and v is None:
+            instance_uuid = uri.split('/')[-1]
+            entity_uuid = uri.split('/')[-3]
+            value = json.loads(value)
+            action = value.get('status')
+            entity_data = value.get('entity_data')
+            # print(type(entity_data))
+            react_func = self.__react(action)
+            if action == 'clean':
                 self.agent.logger.info('__react_to_cache()', ' Native Plugin - This is a remove for URI: {}'.format(uri))
-                instance_uuid = uri.split('/')[-1]
-                entity_uuid = uri.split('/')[-3]
                 self.__force_entity_instance_termination(entity_uuid, instance_uuid)
-            else:
-                instance_uuid = uri.split('/')[-1]
-                entity_uuid = uri.split('/')[-3]
-                value = json.loads(value)
-                action = value.get('status')
-                entity_data = value.get('entity_data')
-                # print(type(entity_data))
-                react_func = self.__react(action)
-                if action == 'clean':
-                    self.__force_entity_instance_termination(entity_uuid, instance_uuid)
-                elif react_func is not None and entity_data is None:
-                    react_func(entity_uuid, instance_uuid)
-                elif react_func is not None:
-                    entity_data.update({'entity_uuid': entity_uuid})
+            elif react_func is not None and entity_data is None:
+                react_func(entity_uuid, instance_uuid)
+            elif react_func is not None:
+                entity_data.update({'entity_uuid': entity_uuid})
 
     def __react(self, action):
         r = {
             'define': self.define_entity,
             'configure': self.configure_entity,
-            'clean': self.clean_entity,
             'stop': self.stop_entity,
             'resume': self.resume_entity,
             'run': self.run_entity
