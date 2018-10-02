07530984eb12d5bb390e2c61ea62bc071b4aeae5
gabrik <gabriele.baldoni@gmail.com>
2018-10-02 09:10:56 +0200

    fixed bug in KVM plugin
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- plugins/KVMLibvirt/KVMLibvirt_plugin.py
+++ plugins/KVMLibvirt/KVMLibvirt_plugin.py
@@ -946,12 +946,10 @@ class KVMLibvirt(RuntimePlugin):
         if url.startswith('http'):
             image_name = os.path.join(self.BASE_DIR, self.IMAGE_DIR, url.split('/')[-1])
             self.agent.get_os_plugin().download_file(url, image_name)
-
-        elif url.image_url.startswith('file://'):
+        elif url.startswith('file://'):
             image_name = os.path.join(self.BASE_DIR, self.IMAGE_DIR, url.image_url.split('/')[-1])
             cmd = 'cp {} {}'.format(url[len('file://'):], image_name)
             self.agent.get_os_plugin().execute_command(cmd, True)
-
         manifest.update({'path': image_name})
         uri = '{}/{}'.format(self.HOME_IMAGE, manifest.get('uuid'))
         self.__update_actual_store(uri, manifest)
