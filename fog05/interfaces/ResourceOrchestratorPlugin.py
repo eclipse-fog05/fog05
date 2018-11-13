e2a96d54b1a1b3717eddfd4efa4ca36eba33f300
gabrik <gabriele.baldoni@gmail.com>
2018-11-13 09:16:46 +0100

    fix RO class
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- fog05/interfaces/ResourceOrchestratorPlugin.py
+++ fog05/interfaces/ResourceOrchestratorPlugin.py
@@ -15,10 +15,10 @@
 from fog05.interfaces import Plugin
 
 
-class ResourceManagmentPlugin(Plugin):
+class ResourceOrchestratorPlugin(Plugin):
 
     def __init__(self, version, plugin_uuid=None):
-        super(ResourceManagmentPlugin, self).__init__(version, plugin_uuid)
+        super(ResourceOrchestratorPlugin, self).__init__(version, plugin_uuid)
 
     def onboard_application(self, application_uuid, application_manifest):
         raise NotImplemented
