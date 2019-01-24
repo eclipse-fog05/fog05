833c78a337b81acab988af421e661f4d08975d97
josrolgil <jmroldangil@gmail.com>
2019-01-24 15:34:01 +0100

    Signed-off-by: josrolgil <jmroldangil@gmail.com>
    Moved one file from utils lxd to brctl because it was used there

--- plugins/LXD/utils.py
+++ plugins/LXD/utils.py
@@ -1,9 +1,2 @@
-def get_bridge_names_from_output_string(output):  # todo: last item is empty-> delete
-    rows = output.split('\n')
-    table = [row.split('\t') for row in rows]
-    bridges_names = [item[0] for item in table]
-    return bridges_names[1:]  # pop name of the table
-
-
 def get_bridge_names_from_instance_networks(instance_networks):
     return [network['br_name'] for network in instance_networks]
