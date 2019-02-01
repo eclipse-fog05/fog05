5ed238899d5dbc0ad287a6083211b4b085e4ad4e
josrolgil <jmroldangil@gmail.com>
2019-02-01 09:11:27 +0100

    Completed first features of OVS plugin including create/delete bridge and ports and set controller Also fog05 automatically creates the bridges if indicated in manifest
    Signed-off-by: josrolgil <jmroldangil@gmail.com>

--- plugins/OVS/OVS_plugin.py
+++ plugins/OVS/OVS_plugin.py
@@ -1,4 +1,6 @@
+import json
 import os
+import uuid
 
 from src.python.fog05.interfaces.NetworkPlugin import NetworkPlugin, BridgeNotExistingException, \
     InterfaceNotExistingException
@@ -20,7 +22,39 @@ class OVS(NetworkPlugin):
         file_dir = os.path.dirname(__file__)
         self.DIR = os.path.abspath(file_dir)
         self.overlay_interface = None
-        #TODO
+
+        if self.agent.get_os_plugin().dir_exists(self.BASE_DIR):
+            if not self.agent.get_os_plugin().dir_exists(os.path.join(self.BASE_DIR, self.DHCP_DIR)):
+                self.agent.get_os_plugin().create_dir(os.path.join(self.BASE_DIR, self.DHCP_DIR))
+        else:
+            self.agent.get_os_plugin().create_dir(self.BASE_DIR)
+            self.agent.get_os_plugin().create_dir(os.path.join(self.BASE_DIR, self.DHCP_DIR))
+
+        if self.configuration.get('data_subnet'):
+            cird = self.configuration.get('data_subnet')
+
+            data = json.loads(self.agent.astore.get(self.agent.ahome))
+            for n in data.get('network'):
+                intf_cird = self.__ip_mask_to_cird(
+                    n.get('inft_configuration').get('ipv4_address'),
+                    n.get('inft_configuration').get('ipv4_netmask')
+                )
+                if cird == intf_cird:
+                    self.overlay_interface = n.get('intf_name')
+
+        '''
+        should listen on:
+
+        - //dfos/<sys-id>/<node-id>/network/<myuuid>/networks/**
+        - //dfos/<sys-id>/<node-id>/network/<myuuid>/bridges/**
+        - //dfos/<sys-id>/<node-id>/network/<myuuid>/interfaces/**
+
+        '''
+
+        uri = '{}/{}/networks/**'.format(self.agent.dhome, self.HOME)
+        self.agent.dstore.observe(uri, self.__react_to_cache_networks)
+        self.agent.logger.info(
+            'startRuntime()', ' bridge-utils Plugin - Observing {}'.format(uri))
 
     def create_virtual_bridge(self, name, uuid):
         command = 'sudo ovs-vsctl add-br {}'.format(name)
@@ -80,14 +114,39 @@ class OVS(NetworkPlugin):
             raise BridgeNotExistingException('{} bridge not exists'.format(bridge_uuid))
         return bridge_name
 
-    def create_bridges_if_not_exist(self, expected_bridges):
-        current_bridges = self.get_virtual_bridges_in_node()
-        cmd = 'sudo sh -c \'ovs-vsctl show | grep Bridge\''
+    def get_virtual_bridges_in_node(self):
+        cmd = 'sudo ovs-vsctl show' #TODO: could be a better command?
         output = self.agent.get_os_plugin().execute_command(cmd)
         return self.get_bridge_names_from_command_output(output)
 
+    def create_bridges_if_not_exist(self, expected_bridges):
+        current_bridges = self.get_virtual_bridges_in_node()
+        for bridge in expected_bridges:
+            if bridge not in current_bridges:
+                self.create_virtual_bridge(bridge, '{}'.format(uuid.uuid4()))
+
     def get_bridge_names_from_command_output(self, output):
-        raise NotImplementedError
+        pattern = 'Bridge'
+        output = output.split('\n')
+        return [self.get_bridge_name_from_row(item) for item in output if self.row_has_bridge_name(item)]
+
+    def get_bridge_name_from_row(self, row):
+        return row.split('"')[1]
+
+    def row_has_bridge_name(self, row):
+        return True if row.find('Bridge')>-1 else False
 
     def execute(self, command):
         return self.agent.get_os_plugin().execute_command(command)
+
+    def __react_to_cache_networks(self, key, value, v):
+        self.agent.logger.info('__react_to_cache_networks()',
+                               ' BRCTL Plugin - React to to URI: {} Value: {} Version: {}'.format(key, value, v))
+        uuid = key.split('/')[-1]
+        value = json.loads(value)
+        action = value.get('status')
+        react_func = self.__react(action)
+        if action == 'undefine':
+            self.delete_virtual_network(uuid)
+        if react_func is not None and action != 'undefine':
+            react_func(**value)
