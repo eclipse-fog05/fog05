3d16d950d435f3b0a369896dc64c759597d2f119
gabrik <gabriele.baldoni@gmail.com>
2019-03-13 11:47:11 +0100

    updated fos_install.sh
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- fos_install.sh
+++ fos_install.sh
@@ -5,7 +5,8 @@ cd fog05
 git checkout 0.2-devel
 
 sudo apt update -qq
-sudo apt install libev libssl -y
+sudo apt install libev4 libssl1.0.0 pyhton3-pip python3-dev -y
+sudo pip3 install jsonschema
 
 mkdir -p src/ocaml/_build/default/src/fos/fos-agent/
 curl -L -o /tmp/fos.tar.gz https://www.dropbox.com/s/yxmcomji7pezq8h/fos.tar.gz
