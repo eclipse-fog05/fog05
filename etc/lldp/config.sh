310506779f1fe0bff1557f5ca705660b0ea2dfe1
Gabriele Baldoni <gabrik@users.noreply.github.com>
2019-03-26 13:00:30 +0100

    LLDP Neighbors discovery implementation (#52)
    ## Agent + OS Plugin
    A node can provide his network neighbors discoverd using LLDP, this feature uses lldpd version 1.0.3 that can be installed and configured using the fog05's makefile.
    
    Discovery can expose also other networking hardware connected to the node that is LLDP capable (eg. router and switches), thus allowing the possibility to construct a network graph for the fog05 deployment.

---
+++ etc/lldp/config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILE="lldpd.conf"
+
+DIR="/etc/fos/lldpd/"
+cd $DIR
+
+if [ -f "$FILE" ]; then
+	rm $FILE
+fi
+
+echo "configure system chassisid $(cat /etc/machine-id)" >> $FILE
+echo "configure system hostname $(hostname)" >> $FILE
+echo "configure system description fog05" >> $FILE
+echo "configure lldp portidsubtype macaddress" >> $FILE
+
+#echo "configure system interface pattern $IFACE" >> $FILE
+for IFACE in $(ls /sys/class/net/ | grep -v lo); do
+	echo "configure ports $IFACE lldp portdescription $IFACE" >> $FILE
+done
