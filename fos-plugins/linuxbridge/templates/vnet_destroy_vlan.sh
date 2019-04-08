c6a501190dbce4c85a4685c3dc8c316c1f5098fc
Gabriele Baldoni <gabrik@users.noreply.github.com>
2019-04-08 13:55:27 +0200

    Global hashing functions for VxLAN IDs and VLAN IDs, virtual networks can be connected to physical VLAN (#58)
    ## Global hashing functions for VxLAN IDs and VLAN IDs
    All nodes are able to select the same VxLAN ID or VLAN ID for a virtual network using a global hash function, this function uses the uuid in order to generate the VNI or VLAN ID.
    
    ## Virtual Networks can be connected to physical VLANs
    The LinuxBridge plugin is able to attach the containers to physical VLANs, new parameters can be set in the LinuxBridge configuration in order to allow the vlan id range and the interface to be used for VLANs.

---
+++ fos-plugins/linuxbridge/templates/vnet_destroy_vlan.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+# Copyright (c) 2014,2019 ADLINK Technology Inc.
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+#
+# Contributors: Gabriele Baldoni, ADLINK Technology Inc. - OCamk plugins set
+
+
+sudo ip link set br-{{ net_id }} down
+sudo ip link del br-{{ net_id }}
+sudo ip netns del fosns-{{ net_id }}
+
+
