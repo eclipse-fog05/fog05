8dd4c455f5dc17fd41d128a9ef75dfb80d48ce92
Gabriele Baldoni <gabrik@users.noreply.github.com>
2019-04-10 10:56:01 +0200

    Systemd Integration (#61)
    ## Systemd integration
    It is now possible to use systemd to start Eclipse fog05 agent and plugins, this allow also autostart at startup.

---
+++ etc/systemd/enable.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo systemctl enable yaks
+sudo systemctl enable fos_agent
+sudo systemctl enable fos_linux
+sudo systemctl enable fos_linuxbridge
+sudo systemctl enable fos_lxd
