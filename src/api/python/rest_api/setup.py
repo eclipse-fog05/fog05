745456da3ea5fd9895dd42049c65db0dbd8764a5
gabrik <gabriele.baldoni@gmail.com>
2019-04-24 12:24:56 +0200

    Added MEC platform with standart Mp1 interface, update to latest yaks api
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- src/api/python/rest_api/setup.py
+++ src/api/python/rest_api/setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='fog05rest',
-    version='0.0.1',
+    version='0.0.2',
     author='ADLINK',
     packages=['fog05rest',],
     install_requires=['requests'],
