e36432dde076716c8ae60f1e0b65a23a7a5598f7
gabrik <gabriele.baldoni@gmail.com>
2019-05-16 16:36:12 +0200

    fix in install file
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- install_fos_lxd.sh
+++ install_fos_lxd.sh
@@ -22,8 +22,7 @@ lxc network create lxdbr0 ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6
 EONG
 
 
-
-sudo pip3 install jsonschema
+sudo pip3 install jsonschema psutil jinja2
 
 mkdir -p src/agent/_build/default/fos-agent
 
