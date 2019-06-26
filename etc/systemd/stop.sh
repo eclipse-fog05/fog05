b7ab204e0dc2ad5f16aa04f98adafa40d330762b
Gabriele Baldoni <gabrik@users.noreply.github.com>
2019-06-26 15:41:30 +0200

    Fixes in LXD plugin, LinuxBridge plugin, APIs (#114)
    * addition in yaks connectors, both ocaml and pyhton
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * added eval calls in agent for floating ip and port connection to network
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * typo fix in fagent
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * fix yaks_connector.py, was not using updated resource tree for fdu
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * added utility function for getting ip address of a given connection point
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * added logs in lxd_plugin when generating custom profile device configuration
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * fix in lxd monitoring
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- etc/systemd/stop.sh
+++ etc/systemd/stop.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-
-sudo systemctl stop yaks
-sudo systemctl stop fos_agent
-sudo systemctl stop fos_linux
-sudo systemctl stop fos_linuxbridge
 sudo systemctl stop fos_lxd
+sudo systemctl stop fos_linuxbridge
+sudo systemctl stop fos_linux
+sudo systemctl stop fos_agent
+sudo systemctl stop yaks
+
+
+
