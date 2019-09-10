c534312664ac5514a7fe6d4838b60a3cd24ee17d
gabrik <gabriele.baldoni@gmail.com>
2019-09-10 16:28:30 +0200

    Added script to start with screen
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- etc/screen_start.sh
+++ etc/screen_start.sh
@@ -2,7 +2,7 @@
 
 screen -S yaks -dm bash -c 'sudo -u fos yaksd -vv'
 sleep 1
-screen -S rest -dm bash -c 'sudo -u fos python3 /etc/fos/rest/service.py /etc/fos/rest/service.json '
+screen -S rest -dm bash -c 'sudo -u fos YAKS_HOST=127.0.0.1 python3 /etc/fos/rest/service.py /etc/fos/rest/service.json '
 sleep 1
 screen -S agent -dm bash -c 'sudo -u fos /etc/fos/agent -c /etc/fos/agent.json -v'
 sleep 1
